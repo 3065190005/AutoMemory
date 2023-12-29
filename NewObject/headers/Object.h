1e4d7876b699be707008ed23a66e6427fc55e823
unknown <3065190005@qq.com>
2023-12-30 02:17:03 +0800

    add using namespace

---
+++ NewObject/headers/Object.h
@@ -0,0 +1,10 @@
+#include "CerObjectDef.h"
+#include "CerObjectDebug.h"
+#include "CerObjectManager.h"
+#include "CerObject.h"
+
+using namespace ScriptC::Obj;
+
+using object = CerObject;
+using debug = CerObjectDebug;
+using objectManager = CerObjectManager;
